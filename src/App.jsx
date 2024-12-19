bf21ba1bc102f1b1b8f7bc362ef7653ef3456931
arun <arunps880@gmail.com>
2024-12-20 00:42:12 +0530

    product upload update

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ const App = () => {
   const [token, setToken] = useState(
     localStorage.getItem("token") ? localStorage.getItem("token") : ""
   );
-  console.log(token);
 
   useEffect(() => {
     localStorage.setItem("token", token);
@@ -33,7 +32,7 @@ const App = () => {
 
             <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
               <Routes>
-                <Route path="/add" element={<AddProduct />} />
+                <Route path="/add" element={<AddProduct token={token}/>} />
                 <Route path="/list" element={<ListProduct />} />
                 <Route path="/orders" element={<ProductOrders />} />
               </Routes>
